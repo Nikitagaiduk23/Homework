c401103c7d54c137b7fb295c034145ad5062839e
NikitGaiduk31 <n0lppa23@gmail.com>
2024-10-13 12:42:15 +0300

    make coments

--- modules/request_api.py
+++ modules/request_api.py
@@ -1,21 +1,21 @@
 import requests
-#Імпортуваня бібліотеки Requests
+#Імпортуваня бібліотеки Requests 
 from .read_json import read_json
-#Імпортує із файла read_json функцію read_json
+#Імпортує функцію для читання json файлів
 import json
 #Імпортуваня json
 data_api = read_json(name_file= 'config_api.json')
-#Загружає вміст файла config_api (?)
+#Копіювання вміста файла config_api 
 API_KEY = data_api['api_key']
-#Змінна зі значенням api ключа
+#Константа зі значенням api ключа
 CITY_NAME = data_api['city_name']
-#Змінна з ім'ям міста
+#Константа з ім'ям міста
 URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}"
-#Посилання на погоду 
+#Константа з посиланням у яку ми передаемо константи CITY_NAME та API_KEY 
 response = requests.get(URL)
-#
+#отримує дані за посиланням URL
 if response.status_code == 200:
     #Умова яка перевіряє чи були отрімані данні
     data_dict = json.loads(response.content)
-    #Зберігаемо данні о погоді (?)
+    #Перетворюе байти у текст
     print(json.dumps(data_dict, indent= 4))
